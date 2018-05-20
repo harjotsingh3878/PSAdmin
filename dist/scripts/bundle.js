003fa1417681e8d8cb16bdb00f6d7d486bedc467
Harjot Singh <hsingh@conceptinc.ca>
2018-05-19 20:08:37 -0400

    added lint

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -10367,6 +10367,8 @@ return jQuery;
 },{}],2:[function(require,module,exports){
 $ = jQuery = require('jquery');
 
+test = 1;
+
 var App = console.log('Hello world from Browserify');
 
 module.exports = App;
